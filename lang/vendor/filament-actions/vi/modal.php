e576b28238d53402f20b3ce711a9475aa8842813
nfuongtech <154567240+nfuongtech@users.noreply.github.com>
2025-10-22 22:00:52 +0700

    align filters row and localize action labels

---
+++ lang/vendor/filament-actions/vi/modal.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'heading' => 'Xác nhận hành động',
+    'actions' => [
+        'cancel' => [
+            'label' => 'Hủy',
+        ],
+        'confirm' => [
+            'label' => 'Gửi đi',
+        ],
+        'submit' => [
+            'label' => 'Gửi đi',
+        ],
+    ],
+];
---
+++ lang/vendor/filament-tables/vi/filters.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'heading' => 'Bộ lọc',
+    'buttons' => [
+        'reset' => [
+            'label' => 'Đặt lại',
+        ],
+    ],
+    'form' => [
+        'actions' => [
+            'cancel' => [
+                'label' => 'Hủy',
+            ],
+            'reset' => [
+                'label' => 'Đặt lại',
+            ],
+            'submit' => [
+                'label' => 'Gửi đi',
+            ],
+        ],
+    ],
+];
